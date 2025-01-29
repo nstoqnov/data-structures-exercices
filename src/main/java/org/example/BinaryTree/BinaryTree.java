b3c4a3a8523a085d82e3ad7344c888556e75592c
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-01-29 17:17:36 +0200

    add delete methods for bt

--- src/main/java/org/example/BinaryTree/BinaryTree.java
+++ src/main/java/org/example/BinaryTree/BinaryTree.java
@@ -36,18 +36,40 @@ public class BinaryTree {
     public boolean containsNode(int value){
         return containsNodeRecursive(root, value);
     }
-    private Node deleteRecursion(Node current, int value){
+    private Node deleteRecursive(Node current, int value){
         if(current == null){
             return null;
         }
-        if(current.value = value){
+        if(current.value == value){
             //delete node
+            //1st case: check if node for deletion has no children
+            //2nd case: check if node has only one child
+            //3rd case: node has 2 children
+            if(current.right == null && current.left == null){
+                return null;
+            }else if (current.right == null && current.left != null){
+                return current.left;
+            } else if (current.left == null && current.right != null) {
+                return current.right;
+            }else{
+                //find smallest value
+                int smallestValue = findSmallestValue(current);
+                current.value = smallestValue;
+                current.right = deleteRecursive(current.right, smallestValue);
+                return current;
+            }
         }
         if(current.value > value){
-            current.left = deleteRecursion(current.left, value);
+            current.left = deleteRecursive(current.left, value);
         }else{
-            current.right = deleteRecursion(current.right,value);
+            current.right = deleteRecursive(current.right,value);
         }
         return current;
+    }
+    private int findSmallestValue(Node root) {
+        return root.left == null ? root.value : findSmallestValue(root.left);
+    }
+    public void delete(int value) {
+        root = deleteRecursive(root, value);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.example.BinaryTree.BinaryTree;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BinaryTree bt = new BinaryTree();
+
+        bt.add(6);
+        bt.add(4);
+        bt.add(8);
+        bt.add(3);
+        bt.add(5);
+        bt.add(7);
+        bt.add(9);
+
+        bt.delete(9);
+        bt.delete(8);
+
+        bt.delete(4);
+
     }
 }
